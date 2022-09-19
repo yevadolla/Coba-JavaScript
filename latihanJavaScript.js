1e43e98b5d4b89f9941bf314cfe3595b75160ccb
yevadolla <yevadolla@gmail.com>
2022-09-19 14:55:44 +0700

    coba function

--- latihanJavaScript.js
+++ latihanJavaScript.js
@@ -1,1 +1,11 @@
 alert('Heloo!')
+
+function jumlahDuaBilangan(a, b){
+    var total;
+    total = a + b;
+
+    return total;
+}
+alert(jumlahDuaBilangan(1,2));
+alert(jumlahDuaBilangan(25,30));
+alert(jumlahDuaBilangan(1500,17.5));
