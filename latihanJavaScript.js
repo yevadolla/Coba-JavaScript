ce47dff6a5a6ee44bf7f7886103897595615ea21
yevadolla <yevadolla@gmail.com>
2022-09-25 19:17:03 +0700

    coba consul log

--- latihanJavaScript.js
+++ latihanJavaScript.js
@@ -10,3 +10,13 @@ alert(jumlahDuaBilangan(1,2));
 function myFunction(){
     document.getElementById("demo4").innerHTML = "Paragraph changed.";
 }
+var a = 8;
+var b = 3;
+var volumA, volumB, total;
+
+volumA = a*a*a;
+volumB = b*b*b;
+
+total = volumA + volumB;
+
+console.log(total);
